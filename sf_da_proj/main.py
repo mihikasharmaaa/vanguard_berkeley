d5ddbca81302757e0beef101c897bf52ccfd47af
Mihika Sharma <mihikasharma@berkeley.edu>
2022-03-31 15:07:17 -0700

    second update:

--- sf_da_proj/main.py
+++ sf_da_proj/main.py
@@ -1,11 +1,8 @@
 from newspaper import Article
- 
-url = "https://www.bloomberg.com/news/articles/2020-08-01/apple-buys-startup-to-turn-iphones-into-payment-terminals?srnd=premium"
- 
-# download and parse article
+
+url = 'https://web.archive.org/web/20220122072147/https://www.sfchronicle.com/health/article/UCSF-scientists-detect-fluid-anomolies-in-people-16784623.php'
 article = Article(url)
 article.download()
 article.parse()
- 
-# print article text
 print(article.text)
+
--- sf_da_proj/test.py
+++ sf_da_proj/test.py
@@ -1,56 +1,58 @@
+from calendar import month
 from gc import collect
 from urls import month_urls
 import requests
 from bs4 import BeautifulSoup
+from newspaper import Config
+from newspaper import Article
 
 all_urls = []
 def collect_urls(month_urls):
-    for i in range(len(month_urls)): #for each news page in list
-        url = month_urls[i] #gets the page url for that date
+    for url in month_urls: #for each news page in list
         page = requests.get(url)
-        html = page.text #turns it into its html
-        soup = BeautifulSoup(html, 'html.parser')
+        soup = BeautifulSoup(page.text, 'html.parser')
         urls_on_page = [] #initializes list of all urls on that page
-        for link in soup.find_all('a'): #for each link on the page
-            curr_url = link.get('href') #get the url
-            str_url = str(curr_url)
-            if 'article' in str_url: #filtering noise, if it is a link to an article
-                if str_url[0:4] == '/web':
-                    str_url = 'https://web.archive.org' + str_url
-                urls_on_page.append(str_url) #add to list of all urls on the page
+        x = soup.find_all('a')
+        urls_on_page = [str(link.get('href')) for link in x if 'article' in str(link.get('href'))]
+        for u in range(len(urls_on_page)):
+            old_u = urls_on_page[u]
+            if old_u[0:4] == '/web':
+                new_u = 'https://web.archive.org' + old_u
+                urls_on_page[u] = new_u
         all_urls.append(urls_on_page) #add list to list of all urls total for the month
-
 collect_urls(month_urls)
 
-def analyze_text(all_urls):
+def analyze_day(day_urls):
     crime = False
     Sf = False
     Oak = False
     Da = False
     San_DA = 0
     Oak_DA = 0
-    for i in range(len(all_urls)):
-        day_urls = all_urls[i] # operating on one day at a time
-        for i in range(len(day_urls)): #operating on one url at a time
-            url = day_urls[i]
-            page = requests.get(url)
-            html = page.text #turns it into its source code
-            soup = BeautifulSoup(html, 'html.parser')
-            for p in soup.find_all('p'): #each paragraph in article
-                if('crime' in p):
-                    crime = True
-                if('San Francisco' in p):
-                    Sf = True
-                if('Oakland' in p):
-                    Oak = True
-                if('District Attorney' in p):
-                    Da = True
-            if(Sf and crime and Da):
-                San_DA += 1
-            if(Oak and crime and Da):
-                Oak_DA +=1
+    for u in day_urls:
+        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.109 Safari/537.36'
+        config = Config()
+        config.browser_user_agent = user_agent
+        config.request_timeout = 15
+        article = Article(u, config=config)
+        article.download()
+        article.parse()
+        full_article = article.text
+        full_article = str(full_article).lower().replace('san francisco chronicle', '')
+        if(full_article.find('crime') != -1 or full_article.find('police') != -1):
+            crime = True
+        if(full_article.find('san francisco') != -1):
+            Sf = True
+        if(full_article.find('oakland') != -1):
+            Oak = True
+        if(full_article.find('district attorney') != -1 or full_article.find('d.a.') != -1):
+            Da = True
+        if(Sf and crime and Da):
+            San_DA += 1
+        if(Oak and crime and Da):
+            Oak_DA +=1
     return San_DA, Oak_DA
-                
-print(analyze_text(all_urls))
 
-
+print(analyze_day(month_urls[0]))
+##results = map(analyze_day, all_urls)
+##print(list(results))
